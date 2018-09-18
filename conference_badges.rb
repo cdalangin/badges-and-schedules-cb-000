5b51f2382ca9b62100d1c4cd5295217e5a768b3a
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-18 13:37:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator
+end
+
+def assign_rooms
+  group = []
+  group.each_with_index do |name, index|
+  group[name] = index
+  "Hello, #{name}! You'll be assigned to room #{index}!"
+end
+end
