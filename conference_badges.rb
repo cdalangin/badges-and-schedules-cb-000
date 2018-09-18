2f13f91b05443f22cbc95b8b2606bba044b44862
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-18 14:54:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(attendees)
+  badges = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 end
 
 def assign_rooms(attendees)
