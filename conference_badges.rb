1e2beb3c9cebcf4dd7042ddcf34b9895ebfe8c96
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-18 15:07:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,10 +4,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(name)
   badges = []
   array.each do |name|
-  puts "Hello, my name is #{name}"
+  badges << "Hello, my name is #{name}."
 end
 badges
 end
